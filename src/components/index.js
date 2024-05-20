d0e91d5b7239813639287275f5cb86c243ca9a8f
abutalhasheikh33 <abutalhasheikh33@gmail.com>
2024-05-20 23:42:57 +0530

    PostCard/SelectcomponentAdded

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,10 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./Container/Container"
 import LogoutBtn from "./Header/LogoutBtn"
+import Button from "./Button"
+import PostCard from "./PostCard"
+import Select from "./Select"
+import Input from "./Input"
+import Logo from "./Logo"
 
-
-export {Header,Footer,Container,LogoutBtn};
+export {Header,Footer,Container,LogoutBtn,Button,Input,Logo,PostCard,Select};
